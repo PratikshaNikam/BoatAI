fffc293526ffae06b25fa3445efce838f014ee48
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2025-01-02 23:32:28 +0530

    components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+
+
+
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../src/Components/Sidebar/Sidebar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import {getThemePallete} from './theme/ThemePallete';
+import {Grid} from '@mui/material';
+import { ThemeContext } from '@emotion/react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState(localStorage.getItem('theme') || 'light');
+  const [chat, setChat] = useState([]);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const theme=React.useMemo(() => createTheme(getThemePallete(mode)), [mode]);
+
+  useEffect(() => {
+    localStorage.setItem('theme', mode);
+  
+  },[mode])
+    
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeContext.Provider value={{ mode: mode, setMode: setMode }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Grid container sx={{ background: 'linear-gradient(rgba(215,199,244,0.2),rgba(151,133,186,0.2))' }}>
+          
+          <Grid
+            item xs={12} ms={2.5}
+            sx={{
+              bgcolor: 'primary.light',
+              '@media (max-width:800px)': {
+                width: '70%',
+                transform: menuOpen ? 'translateX(0)' : 'translateX(-100%)',
+                transition:'transform 400ms ease',
+              }
+            }}
+            position={{ xs: 'fixed', ms: 'relative' }}
+            height={'100vh'}
+            zIndex={{ xs: 9999, md: 1 }}
+            boxShadow={{xs:menuOpen ? 10:0,md:0}}
+          >
+            <Sidebar setChat={setChat} closeMenu={() => setMenuOpen(false)}></Sidebar>
+            
+
+          </Grid>
+
+          <Grid item xs={12} md={ 9.5}
+          >
+            <Outlet context={{chat:chat,setChat:setChat,handleMobileMenu:setMenuOpen}} />
+
+          </Grid>
+
+        </Grid>
+
+        
+
+      </ThemeProvider>
+      
+   </ThemeContext.Provider>
   )
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import Sidebar from "../../Components/Sidebar/Sidebar";
+
+export default function Home() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
